53adf40eebab1d2340730379b4951413fd86da39
Stas <525210@gmail.com>
2022-01-31 18:14:31 +0200

    4 Создание API на Laravel

--- app/Http/Controllers/Api/DeskController.php
+++ app/Http/Controllers/Api/DeskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DeskResource;
 use App\Models\Desk;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class DeskController extends Controller
      */
     public function index()
     {
-        return Desk::all();
+        return DeskResource::collection(Desk::with('lists')->get());
     }
 
     /**
@@ -26,7 +27,7 @@ class DeskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $created_desk = Desk::create($request->all());
     }
 
     /**
@@ -37,7 +38,7 @@ class DeskController extends Controller
      */
     public function show($id)
     {
-        return Desk::find($id);
+        return new DeskResource(Desk::with('lists')->findOrFail($id));
     }
 
     /**
